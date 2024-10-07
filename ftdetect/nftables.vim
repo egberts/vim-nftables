egberts/vim-nftables

commit af699d87c79e1a798a9a93d2dbe8c207bd20a660

Waypoint 1

ftdetect/nftables.vim
@@ -1,3 +1,5 @@
+#
+# Detect file type by use of shebang
 function! s:DetectFiletype()
     if getline(1) =~# '^#!\s*\%\(/\S\+\)\?/\%\(s\)\?bin/\%\(env\s\+\)\?nft\>'
         setfiletype nftables
@@ -6,6 +8,9 @@ endfunction
 
 augroup nftables
     autocmd!
+    # Detect file type by use of shebang
     autocmd BufRead,BufNewFile * call s:DetectFiletype()
+
+    # Detect file type by its filetype
     autocmd BufRead,BufNewFile *.nft,nftables.conf setfiletype nftables
 augroup END

test/blackhole.nft
@@ -1,0 +1,3 @@
+
+element filter blackhole { 192.168.0.1, 192.168.0.10 }
+

test/error-syntaxes.nft
@@ -1,0 +1,45 @@
+#!/usr/sbin/nft -f
+# File: all-syntaxes.nft
+# Description: A test NFT file to exercise all Vim syntax highlightings.
+#
+# This is the part where you can add your own esoteric setting(s) if it breaks
+# the vim syntax highlighter for NFTABLES (yet `nft` takes it in just normally).
+# TODO: More todo, FIXME, FIXME: TODO: xxx XXX ARGH!
+
+
+#  ERROR SYNTAX (intentional)
+#  They should not be highlighted
+# SYNTAX ERRORS GOES PAST HERE
+
+include Filespec  # intentional syntax error
+include 'Filespec'  # intentional syntax error
+include 'Fil"espec'  # intentional syntax error
+include "Filespec" bad stuff oppsie  # intentional syntax error
+include "Filespec"  oopsie  # intentional syntax error
+include "Filespec"  oopsie  ;  # intentional syntax error
+
+list oopsie  # invalid syntax
+list oopsie ;  # invalid syntax
+list set myIpTable handle 123 # intentional syntax error
+list set inet myInetTable handle 123  # Intentional Syntax ERROR
+list flow tables arp  # flowtables do not support ARP
+list flow tables bridge  # flowtables do not support BRIDGE
+list flow tables netdev  # flowtables do not support NETDEV
+list flow table arp table_name identifier  # flowtables do not support ARP
+list flow table bridge table_name identifier  # flowtables do not support BRIDGE
+list flow table netdev table_name identifier  # flowtables do not support NETDEV
+flush chain myIpTable myIpChain ; flush chain netdev myNetDev X; oopsie
+flush flow table arp table_name identifier  # flowtable do not support ARP
+flush flow table bridge table_name identifier  # flowtable do not support BRIDGE
+flush flow table netdev table_name identifier  # flowtable do not support NETDEV
+list flowtable arp table_name identifier  # flowtable do not support ARP
+list flowtable bridge table_name identifier  # flowtable do not support BRIDGE
+list flowtable netdev table_name identifier  # flowtable do not support NETDEV
+list flowtables arp  # flowtable do not support ARP
+list flowtables bridge  # flowtable do not support BRIDGE
+list flowtables netdev  # flowtable do not support NETDEV
+
+# list ct <ct_obj_type> <obj_spec>
+list ct timeout 15
+
+export ruleset vm json   # no export for singular 'ruleset'

commit 28cf27b3dc61809adf918d778ca67bb5f03c5138

Use " for comments in ftdetect/nftables.vim
The `#` character is only recognized as a comment marker in Vim9 Script.
Since `vim9script` is not set, use `"` for comments.

Fixes: #2
Fixes: af699d87c79e ("Waypoint 1")
Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

ftdetect/nftables.vim
@@ -1,5 +1,5 @@
-#
-# Detect file type by use of shebang
+"
+" Detect file type by use of shebang
 function! s:DetectFiletype()
     if getline(1) =~# '^#!\s*\%\(/\S\+\)\?/\%\(s\)\?bin/\%\(env\s\+\)\?nft\>'
         setfiletype nftables
@@ -8,9 +8,9 @@ endfunction
 
 augroup nftables
     autocmd!
-    # Detect file type by use of shebang
+    " Detect file type by use of shebang
     autocmd BufRead,BufNewFile * call s:DetectFiletype()
 
-    # Detect file type by its filetype
+    " Detect file type by its filetype
     autocmd BufRead,BufNewFile *.nft,nftables.conf setfiletype nftables
 augroup END